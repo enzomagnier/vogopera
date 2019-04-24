3e9def5b50d8c8637583d0843297545ab3e18288
root <root@localhost.localdomain>
2019-04-24 16:08:52 +0200

    vogopera

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Vogopera</title>
+    <link rel="stylesheet" href="index.css">
+  </head>
+  <body>
+    <h3>Vogo</h3>
+    <input id="burger" type="checkbox" />
+
+  <label for="burger">
+      <span></span>
+      <span></span>
+      <span></span>
+  </label>
+  <nav>
+    <h1><i>Vogopera</i></h1>
+    <ul>
+      <li><a href="#">Link #1</a></li>
+      <li><a href="#">Evenement</a></li>
+      <li><a href="#">Link #3</a></li>
+    </ul>
+  </nav>
+  <img src="https://www.montpellier3m.fr/sites/default/files/Hommes_d%27affaires_VDA_0005bis_0.jpg" alt="">
+  </body>
+</html>
