d86e23c7a1bc4f1658eae788d70148670aff76d9
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-04-25 00:25:55 +0200

    fin du travail sur page details

--- details.php
+++ details.php
@@ -7,7 +7,16 @@
     <?php include 'nav.php'; ?>
     <main>
         <a href="#"><img src="images/CARMEN.jpeg" alt=""></a>
-        
+        <h2>Carmen</h2>
+        <section class="sectionDetails">
+            <a href=""><button class="btnDetails">Livret</button></a>
+            <a href=""><button class="btnDetails">Vidéos</button></a>
+        </section>
+
+        <section class="synopsisDetails">
+            <h3>Synopsis</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aut, dolor quas repudiandae dolore enim similique debitis illum adipisci, obcaecati soluta neque sed nisi a labore vel ut accusantium sint.</p>
+        </section>
     </main>
     <?php include 'footer.php'; ?>
 </body>
