4416002df61b002ec050379d0ca722cc8aaf59ba
root <root@localhost.localdomain>
2019-04-24 16:43:36 +0200

    vogoper

--- index.php
+++ index.php
@@ -17,9 +17,11 @@
   <nav>
     <h1><i>Vogopera</i></h1>
     <ul>
-      <li><a href="#">Link #1</a></li>
+      <li><a href="#">Acceuil</a></li>
       <li><a href="#">Evenement</a></li>
-      <li><a href="#">Link #3</a></li>
+      <li><a href="#">Visite Virtuelle</a></li>
+      <li><a href="#">Live et Virtuelle</a></li>
+
     </ul>
   </nav>
   <img src="https://www.montpellier3m.fr/sites/default/files/Hommes_d%27affaires_VDA_0005bis_0.jpg" alt="">
