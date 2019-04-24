29009760f64d97ca7e359a5ec1071fc1b0a6e95d
simoccjavmonp12 <Phenixstrato@gmail.com>
2019-04-24 23:28:45 +0200

    page programmes fini, j'attaque page details

--- details.php
+++ details.php
@@ -6,8 +6,8 @@
     <?php require_once 'mod.php'; ?>
     <?php include 'nav.php'; ?>
     <main>
-        <a href="details.php"><img src="images/CARMEN.jpeg" alt=""></a>
-        <a href="#"><img src="images/NotreDame.jpeg" alt=""></a>
+        <a href="#"><img src="images/CARMEN.jpeg" alt=""></a>
+        
     </main>
     <?php include 'footer.php'; ?>
 </body>
