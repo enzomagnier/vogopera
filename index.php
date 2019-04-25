c1f49ab241383277a46247b7dc691343fbcd2931
Moustapha <mouhamous22@gmail.com>
2019-04-25 09:52:43 +0200

    pull

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <?php require_once 'mod.php'; ?>
   <?php include 'nav.php'; ?>
   <img src="https://www.montpellier3m.fr/sites/default/files/Hommes_d%27affaires_VDA_0005bis_0.jpg" alt="">
-  <hr></hr>
+  <hr>
 
 <?php include 'footer.php'; ?>
   </body>
